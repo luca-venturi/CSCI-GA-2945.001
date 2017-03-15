36342a506d867f962c505b09f3ac0791491a2d7a
luca-venturi <luca.venturi.92@gmail.com>
2017-03-15 12:02:23 -0400

    jacobi2D modified

--- hw2/jacobi2D.c
+++ hw2/jacobi2D.c
@@ -5,12 +5,12 @@
 
 double norm(int N, double *v);
 
-double *jacobi(int *M, double *eps, int N, double *u, double h);
+void *jacobi(int *M, double *eps, int N, double *u, double h);
 
 int main (int argc, char **argv) /* the program takes as input the number of grid points N and the maximum number of iterations M */
 {
-	int i, N, *M, met;
-	double *f, *u, h0, h, *eps, elapsed;
+	int N, *M;
+	double *u, h, *eps, elapsed;
 	timestamp_type time1, time2;
 
 	if (argc != 3 && argc != 2) {
@@ -18,9 +18,9 @@ int main (int argc, char **argv) /* the program takes as input the number of gri
     	abort();
   	}
 
-	N = atol(argv[1])+2;
-	h0 = N-1;
-	h = (h0*h0);
+	N = atol(argv[1]);
+	h = 1/(double)((N+1)*(N+1));
+	printf("%f", h);
 	M = (int *) malloc(sizeof(int));
 	if (argc == 3) {
 		*M = atol(argv[2]);
@@ -30,7 +30,7 @@ int main (int argc, char **argv) /* the program takes as input the number of gri
 	eps = (double *) malloc(sizeof(double));	
 	*eps = 0.0001;	
 	
-	u = (double *) calloc(N*N, sizeof(double));	
+	u = (double *) calloc((N+2)*(N+2), sizeof(double));	
 	
 	get_timestamp(&time1);
 	jacobi(M, eps, N, u, h);		
@@ -58,36 +58,32 @@ double norm(int N, double *v)
 	return sqrt(sum_v);
 }
 
-double *jacobi(int *M, double *eps, int N, double *u, double h) 
+void *jacobi(int *M, double *eps, int N, double *u, double h) 
 {	
-	int it = 0, max_it, j;
-	double *b, *temp_u, min_eps, s, norm_b0, norm_b=1;
+	int it = 0, max_it, i, j, N2;
+	double *b, *temp_u, min_eps, norm_b0, norm_b=1;
 
 	min_eps = *eps;
 	max_it = *M;
 	norm_b0 = N;
-	N2 = (N+2)**2;
+	N2 = (N+2)*(N+2);
 
 	temp_u = (double *) malloc(N2*sizeof(double));
-	b = (double *) malloc(N2*sizeof(double));
+	b = (double *) calloc(N2, sizeof(double));
 
 	while (it < max_it && norm_b > min_eps) {
-		
-		/* stiamo usando N vero, h vero e N2 = (N+2)^2: cambiare in modo che sia cosi */
-
 		for (i = 0; i<N2; i++) 
-			temp_u[j] = u[j];
+			temp_u[i] = u[i];
 		
 		for (i = 1; i < N+1; i++) {
 			for (j = 1; j < N+1; j++)
-				u[N*i+j] = (h**2 - temp_u[N*(i-1)+j] - temp_u[N*(i+1)+j] - temp_u[N*i+j+1] - temp_u[N*i+j-1])/4;
+				u[N*i+j] = (h - temp_u[N*(i-1)+j] - temp_u[N*(i+1)+j] - temp_u[N*i+j+1] - temp_u[N*i+j-1])/4;
 		}
 
-		/* b[0] = -1 + h*(2*u[0]-u[1]);
-		for (j = 1; j < N-1; j++) {
-			b[j] = -1 + (-u[j-1]+2*u[j]-u[j+1])*h;
+		for (i = 1; i < N+1; i++) {
+			for (j = 1; j < N+1; j++)
+				b[N*i+j] = 1 + (u[N*(i-1)+j] + u[N*(i+1)+j] + u[N*i+j+1] + u[N*i+j-1] - 4*u[N*i+j])/h;
 		}
-		b[N-1] = -1 + h*(2*u[N-1]-u[N-2]); */
 			
 		norm_b = norm(N2, b);
 		norm_b /= norm_b0;
