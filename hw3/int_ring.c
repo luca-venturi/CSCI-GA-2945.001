834ea04d73e4bcdb383250d62589e221e4580aa6
luca-venturi <luca.venturi.92@gmail.com>
2017-03-28 12:51:28 -0400

    int_ring.c first trial

--- hw3/int_ring.c
+++ hw3/int_ring.c
@@ -3,7 +3,8 @@
 
 int main (int argc, char *argv[]) 
 {	
-	int size, rank, N, message = 0, i, k;
+	int size, rank, N, message = 0, i, k, tag = 0;
+	MPI_Status status;
 
 	printf( "Enter parameter N:");
    	scanf("%d", &N);
@@ -13,10 +14,28 @@ int main (int argc, char *argv[])
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
 	for (i = 0; i < N; i++) {
-		for (k = 0; k < size; k++) {	
+		if (i == 0 && rank == 0) {		
+			MPI_Send(&tag, 1, MPI_INT, rank + 1, tag, MPI_COMM_WORLD);
+		} else if (i > 0 && rank == 0) {
+			MPI_Recv(&tag, 1, MPI_INT, size - 1, tag, MPI_COMM_WORLD, &status);
+			tag += 1;
+			MPI_Send(&tag, 1, MPI_INT, rank + 1, tag, MPI_COMM_WORLD);		
+		}
+		for (k = 1; k < size - 1; k++) {	
 			if (rank == k)	{
-				
+				MPI_Recv(&tag, 1, MPI_INT, rank - 1, tag, MPI_COMM_WORLD, &status);
+				tag += 1;
+				MPI_Send(&tag, 1, MPI_INT, rank + 1, tag, MPI_COMM_WORLD);
 			}
+		}
+		if (i < N -1 && rank == size - 1) {
+			MPI_Recv(&tag, 1, MPI_INT, size - 2, tag, MPI_COMM_WORLD, &status);
+			tag += 1;
+			MPI_Send(&tag, 1, MPI_INT, 0, tag, MPI_COMM_WORLD);
+		}
+		else if (i = N -1 && rank == size - 1) {
+			MPI_Recv(&tag, 1, MPI_INT, size - 2, tag, MPI_COMM_WORLD, &status);
+			printf("The last value of tag, received by process %d of %d, is %d.\n", rank, size, tag);
 		}
 	}
 
---
+++ hw3/prova.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main () 
+{
+	int n = 2%2;
+
+	printf("%d", n);
+
+	return 0;
+}
