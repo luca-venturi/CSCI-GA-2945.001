2b136bbdcb4dbd74926e2c04dc2585852251d51f
luca-venturi <luca.venturi.92@gmail.com>
2017-04-14 11:37:23 -0400

    mpi_solved5.c added

---
+++ hw4/mpi_solved5.c
@@ -0,0 +1,64 @@
+/******************************************************************************
+* FILE: mpi_solved5.c
+* Comment: The program was hanging or (depending on the implementation) due to overbuffering on the receveing side. I fixed it including an MPI_Barrier in both the sending and receveing loops. I also changed the while condition to make the program stop at some point.
+******************************************************************************/
+#include "mpi.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MSGSIZE 2000
+
+int main (int argc, char *argv[])
+{
+int        numtasks, rank, i, tag=111, dest=1, source=0, count=0;
+char       data[MSGSIZE];
+double     start, end, result;
+MPI_Status status;
+
+MPI_Init(&argc,&argv);
+MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
+MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+if (rank == 0) {
+  printf ("mpi_solved5 has started...\n");
+  if (numtasks > 2) 
+    printf("INFO: Number of tasks= %d. Only using 2 tasks.\n", numtasks);
+  }
+
+/******************************* Send task **********************************/
+if (rank == 0) {
+
+  /* Initialize send data */
+  for(i=0; i<MSGSIZE; i++)
+     data[i] =  'x';
+
+  start = MPI_Wtime();
+  while (count < 1000) {
+    MPI_Send(data, MSGSIZE, MPI_BYTE, dest, tag, MPI_COMM_WORLD);
+	MPI_Barrier(MPI_COMM_WORLD);
+    if (count % 10 == 0) {
+      end = MPI_Wtime();
+      printf("Count= %d  Time= %f sec.\n", count, end-start);
+      start = MPI_Wtime();
+      }
+	count++;
+    }
+  }
+
+/****************************** Receive task ********************************/
+
+if (rank == 1) {
+  while (count < 1000) {
+    MPI_Recv(data, MSGSIZE, MPI_BYTE, source, tag, MPI_COMM_WORLD, &status);
+	MPI_Barrier(MPI_COMM_WORLD);
+    /* Do some work  - at least more than the send task */
+    result = 0.0;
+    for (i=0; i < 1000000; i++) 
+      result = result + (double)random();
+	count++;
+    }
+  }
+
+MPI_Finalize();
+}
+
